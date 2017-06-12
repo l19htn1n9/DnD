5d067a06da149a2e4050dacecd2656377703fe6c
Freek van der Toorn <ninjafreek@hotmail.com>
2017-06-12 21:16:34 +0200

    Switched to MySql

---
+++ DnD/DnD.CoreApi/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DnD.Models.Repository;
+using DnD.Models.Entities;
+
+namespace DnD.CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        UserRepository userRepo = new UserRepository();
+        // GET api/user/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return userRepo.GetUser(id);
+        }
+    }
+}
