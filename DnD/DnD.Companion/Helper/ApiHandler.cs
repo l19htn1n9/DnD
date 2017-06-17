eb118a56f906e407ac0e1006673a2feb5c0a861e
Freek van der Toorn <ninjafreek@hotmail.com>
2017-06-17 13:17:54 +0200

    Added an activity indicator

--- DnD/DnD.Companion/Helper/ApiHandler.cs
+++ DnD/DnD.Companion/Helper/ApiHandler.cs
@@ -27,41 +27,43 @@ namespace DnD.Companion.Helper
         public void Login(string username, string password)
         {
             try
-            {
-                new Thread(new ThreadStart(() =>{
-
-                    ServicePointManager.DefaultConnectionLimit = 10;
-                    User user = new User { Username = username, Password = password };
-                    string json = JsonConvert.SerializeObject(user);
-                    byte[] data = Encoding.UTF8.GetBytes(json);
-                    Uri uri = new Uri("http://10.0.2.2:5000/api/account/sign-in");
-                    var request = WebRequest.Create(uri);
-                    request.Method = "POST";
-                    request.ContentType = "application/json";
-                    //var request = new HttpWebRequest(new Uri("http://10.0.2.2:5000/api/account/sign-in")) { ContentType = @"application/json", Method = "POST" };
-                    //request.KeepAlive = true;
-                    //var request = new HttpWebRequest(new Uri("http://192.168.0.103:5000/api/account")) { ContentType = @"application/json", Method = "GET" };
-                    request.ContentLength = data.Length;
-                    using (var sw = request.GetRequestStream())
-                    {
-                        sw.Write(data, 0, data.Length);
-                    }
-                    var response = request.GetResponse();
-                    if (response != null)
+            {                   
+                User user = new User { Username = username, Password = password };
+                string json = JsonConvert.SerializeObject(user);
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                Uri uri = new Uri("http://10.0.2.2:5000/api/account/sign-in");
+                var request = WebRequest.Create(uri);
+                request.Method = "POST";
+                request.ContentType = "application/json";
+                //var request = new HttpWebRequest(new Uri("http://10.0.2.2:5000/api/account/sign-in")) { ContentType = @"application/json", Method = "POST" };
+                //request.KeepAlive = true;
+                //var request = new HttpWebRequest(new Uri("http://192.168.0.103:5000/api/account")) { ContentType = @"application/json", Method = "GET" };
+                request.ContentLength = data.Length;
+                using (var sw = request.GetRequestStream())
+                {
+                    sw.Write(data, 0, data.Length);
+                }
+                var response = (HttpWebResponse)request.GetResponse();
+                if (response != null)
+                {
+                    if ((int)response.StatusCode == 200)
                     {
                         var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
                         var returnObject = JsonConvert.DeserializeObject<Token>(responseString);
-                        if(returnObject != null)
+                        if (returnObject != null)
                         {
                             this.AccessToken = returnObject.access_token;
                         }
                     }
-
-                })).Start();
+                    else
+                    {
+                        throw new WebException("Could not login", WebExceptionStatus.UnknownError);
+                    }
+                }
             }
             catch(Exception e)
             {
-
+                throw e;
             }
         }
     }
--- DnD/DnD.Companion/LoginPage.xaml.cs
+++ DnD/DnD.Companion/LoginPage.xaml.cs
@@ -8,16 +8,48 @@ namespace DnD.Companion
 {
     public partial class LoginPage : ContentPage
     {
+
         public LoginPage()
         {
             InitializeComponent();
+            SetEnabled(true);
+            CheckRememberMe();
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
+            SetEnabled(false);
             var username = Username.Text;
             var password = Password.Text;
+            var rememberMe = Switch.IsToggled;
+            if(rememberMe)
+            {
+                Application.Current.Properties["username"] = username;
+                Application.Current.Properties["password"] = password;
+            }
             ApiHandler.Instance.Login(username, password);
+        }
+
+        private void SetEnabled(bool enabled)
+        {
+            Username.IsEnabled = enabled;
+            Password.IsEnabled = enabled;
+            Switch.IsEnabled = enabled;
+            Login.IsEnabled = enabled;
+
+            ActivityIndicator.IsEnabled = !enabled;
+            ActivityIndicator.IsVisible = !enabled;
+        }
+
+        private void CheckRememberMe()
+        {
+            if(Application.Current.Properties.ContainsKey("username") && Application.Current.Properties.ContainsKey("password"))
+            {
+                var username = (string)Application.Current.Properties["username"];
+                var password = (string)Application.Current.Properties["password"];
+                SetEnabled(false);
+                ApiHandler.Instance.Login(username, password);
+            }
         }
     }
 }
